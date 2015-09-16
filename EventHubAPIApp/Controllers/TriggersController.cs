8371fcec13b57eeb8ea892b8d13d49b09aa44ee8
Jeff Hollan <jeffhollan@outlook.com>
2015-09-16 11:35:46 -0700

    made hubname Required

--- EventHubAPIApp/Controllers/TriggersController.cs
+++ EventHubAPIApp/Controllers/TriggersController.cs
@@ -10,6 +10,7 @@ using System.Text;
 using Newtonsoft.Json.Linq;
 using TRex.Metadata;
 using System.Diagnostics;
+using System.ComponentModel.DataAnnotations;
 
 namespace EventHubAPIApp.Controllers
 {
@@ -223,10 +224,11 @@ namespace EventHubAPIApp.Controllers
         public class EventHubActionMessage
         {
             [Metadata("Event Hub Name", null)]
+            [Required(AllowEmptyStrings = false)]
             public string hubName { get; set; }
-
             [Metadata("Message Properties", "Object of Key/Value Properties for message", VisibilityType.Advanced)]
             public string propertiesString { get; set; }
+            
             [Metadata("Message", null)]
             public string message { get; set; }
             [Metadata("Partition Key", null, VisibilityType.Advanced)]
